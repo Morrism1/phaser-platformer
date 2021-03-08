4daa509771c4a948c7fcbc643a6fb29f38cc2bbb
Morrism1 <morrismontana0@gmail.com>
2021-03-08 16:16:09 +0200

    Add the color tweens when player hits obstacles

--- src/scenes/playerController.js
+++ src/scenes/playerController.js
@@ -107,6 +107,28 @@ export default class PlayerController {
 
   spikeHitOnEnter() {
     this.sprite.setVelocityY(-12)
+    const startColor = Phaser.Display.Color.ValueToColor(0xffffff)
+    const endColor = Phaser.Display.Color.ValueToColor(0xff0000)
+    this.scene.tweens.addCounter({
+      from: 0,
+      to: 100,
+      duration: 100,
+      repeat: 2,
+      yoyo: true,
+      ease: Phaser.Math.Easing.Sine.InOut,
+      onUpdate: (tween) => {
+        const value = tween.getValue()
+        const colorObject = Phaser.Display.Color.Interpolate.ColorWithColor(
+          startColor,
+          endColor,
+          100,
+          value
+        )
+        const color = Phaser.Display.Color.GetColor(colorObject.r, colorObject.g, colorObject.b)
+        this.sprite.setTint(color)
+      },
+    })
+
     this.stateMachine.setState('idle')
   }
 
