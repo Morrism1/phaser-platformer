165033dc1fa1093d637e4ffefaae89f91abf456a
Morrism1 <morrismontana0@gmail.com>
2021-03-08 15:36:15 +0200

    Create the default game scene

---
+++ src/scenes/gameScene.js
@@ -0,0 +1,76 @@
+import Phaser from 'phaser'
+import PlayerController from './playerController'
+import ObstaclesController from './obstacles'
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super('game')
+    this.playerController = undefined
+  }
+
+  init() {
+    this.cursors = this.input.keyboard.createCursorKeys()
+    this.obstacles = new ObstaclesController()
+  }
+
+  preload() {
+    this.load.atlas('kim', 'assets/kim.png', 'assets/kim.json')
+    this.load.image('tiles', 'assets/sheet.png')
+    this.load.tilemapTiledJSON('tilemap', 'assets/iceMap.json')
+
+    this.load.image('cherry', 'assets/cherry.png')
+  }
+
+  create() {
+    this.scene.launch('score')
+    const map = this.make.tilemap({ key: 'tilemap' })
+    const tileset = map.addTilesetImage('iceground', 'tiles')
+    const ground = map.createLayer('ground', tileset)
+    ground.setCollisionByProperty({ collides: true })
+    map.createLayer('obstacles', tileset)
+
+    const objLayer = map.getObjectLayer('objects')
+    objLayer.objects.forEach((obj) => {
+      const { x, y, name, width, height = 0 } = obj
+      if (name === 'kim-spawn') {
+        this.kim = this.matter.add
+          .sprite(x + width * 0.5, y, 'kim')
+          .setScale(0.3)
+          .setFixedRotation()
+
+        this.playerController = new PlayerController(this.kim, this.cursors, this.obstacles)
+
+        this.cameras.main.startFollow(this.kim)
+      }
+      if (name === 'cherry') {
+        const cherry = this.matter.add
+          .sprite(x, y, 'cherry', undefined, {
+            isSensor: true,
+            isStatic: true,
+          })
+          .setScale(0.5)
+          .setFixedRotation()
+
+        cherry.setData('type', 'cherry')
+      }
+
+      if (name === 'spikes') {
+        const spike = this.matter.add.rectangle(x + width * 0.5, y + height * 0.5, width, height, {
+          isStatic: true,
+        })
+
+        this.obstacles.add('spikes', spike)
+      }
+    })
+
+    this.matter.world.convertTilemapLayer(ground)
+  }
+
+  update(time, dt) {
+    if (!this.playerController) {
+      return
+    }
+
+    this.playerController.update(dt)
+  }
+}
