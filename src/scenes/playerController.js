f8759e11e381ddd060f332a6e86e10b14cc68a2c
Morrism1 <morrismontana0@gmail.com>
2021-03-11 18:30:11 +0200

    Fix linter errors

--- src/scenes/playerController.js
+++ src/scenes/playerController.js
@@ -1,16 +1,17 @@
-import Phaser from 'phaser';
-import StateMachine from '../stateMachine/StateMachine';
-import { events } from './eventEmitter';
+// @ts-nocheck
+import Phaser from 'phaser'
+import StateMachine from '../stateMachine/StateMachine'
+import { events } from './eventEmitter'
 
 export default class PlayerController {
   constructor(scene, sprite = Phaser.Physics.Matter.Sprite, cursors, obstacles) {
-    this.health = 100;
-    this.sprite = sprite;
-    this.cursors = cursors;
-    this.obstacles = obstacles;
-    this.scene = scene;
-    this.createAnimations();
-    this.stateMachine = new StateMachine(this, 'player');
+    this.health = 100
+    this.sprite = sprite
+    this.cursors = cursors
+    this.obstacles = obstacles
+    this.scene = scene
+    this.createAnimations()
+    this.stateMachine = new StateMachine(this, 'player')
     this.stateMachine
       .addState('idle', { onEnter: this.idleOnEnter, onUpdate: this.idleOnUpdate })
       .addState('walk', { onEnter: this.walkOnEnter, onUpdate: this.walkOnUpdate })
@@ -19,126 +20,126 @@ export default class PlayerController {
       .addState('dead', {
         onEnter: this.deadOnEnter,
       })
-      .setState('idle');
+      .setState('idle')
 
     this.sprite.setOnCollide((data) => {
-      let _a;
-      const body = data.bodyB;
+      let _a
+      const body = data.bodyB
       if (this.obstacles.is('spikes', body)) {
-        this.stateMachine.setState('spike-hit');
-        return;
+        this.stateMachine.setState('spike-hit')
+        return
       }
-      const { gameObject } = body;
+      const { gameObject } = body
       if (!gameObject) {
-        return;
+        return
       }
       if (gameObject instanceof Phaser.Physics.Matter.TileBody) {
         if (this.stateMachine.isCurrentState('jump')) {
-          this.stateMachine.setState('idle');
+          this.stateMachine.setState('idle')
         }
-        return;
+        return
       }
 
-      const sprite = gameObject;
-      const type = sprite.getData('type');
+      const sprite = gameObject
+      const type = sprite.getData('type')
 
       if (type === 'cherry') {
-        events.emit('cherry-collected');
-        sprite.destroy();
+        events.emit('cherry-collected')
+        sprite.destroy()
       }
 
       if (type === 'health') {
-        const value = (_a = sprite.getData('healthValue')) !== null && _a !== void 0 ? _a : 10;
-        this.health = Phaser.Math.Clamp(this.health + value, 0, 100);
-        events.emit('health-changed', this.health);
-        sprite.destroy();
+        const value = (_a = sprite.getData('healthValue')) !== null && _a !== void 0 ? _a : 10
+        this.health = Phaser.Math.Clamp(this.health + value, 0, 100)
+        events.emit('health-changed', this.health)
+        sprite.destroy()
       }
-    });
+    })
   }
 
   update(dt) {
-    this.stateMachine.update(dt);
+    this.stateMachine.update(dt)
   }
 
   setHealth(value) {
-    this.health = Phaser.Math.Clamp(value, 0, 100);
-    events.emit('health-changed', this.health);
+    this.health = Phaser.Math.Clamp(value, 0, 100)
+    events.emit('health-changed', this.health)
 
     if (this.health <= 0) {
-      this.stateMachine.setState('dead');
+      this.stateMachine.setState('dead')
     }
   }
 
   deadOnEnter() {
-    this.sprite.play('player-die');
-    this.sprite.setOnCollide(() => {});
+    this.sprite.play('player-die')
+    this.sprite.setOnCollide(() => {})
     this.scene.time.delayedCall(1500, () => {
-      this.scene.scene.start('game-over');
-      this.setHealth(100);
-    });
+      this.scene.scene.start('game-over')
+      this.setHealth(100)
+    })
   }
 
   idleOnEnter() {
-    this.sprite.play('player-idle');
+    this.sprite.play('player-idle')
   }
 
   idleOnUpdate() {
     if (this.cursors.left.isDown || this.cursors.right.isDown) {
-      this.stateMachine.setState('walk');
+      this.stateMachine.setState('walk')
     }
-    const jumpPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space);
+    const jumpPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space)
 
-    const pressed = jumpPressed || this.cursors.up.isDown;
+    const pressed = jumpPressed || this.cursors.up.isDown
     if (pressed) {
-      this.stateMachine.setState('jump');
+      this.stateMachine.setState('jump')
     }
   }
 
   walkOnEnter() {
-    this.sprite.play('player-run');
+    this.sprite.play('player-run')
   }
 
   walkOnUpdate() {
-    const spriteSpeed = 5;
+    const spriteSpeed = 5
     if (this.cursors.left.isDown) {
-      this.sprite.flipX = true;
-      this.sprite.setVelocityX(-spriteSpeed);
+      this.sprite.flipX = true
+      this.sprite.setVelocityX(-spriteSpeed)
     } else if (this.cursors.right.isDown) {
-      this.sprite.flipX = false;
-      this.sprite.setVelocityX(spriteSpeed);
+      this.sprite.flipX = false
+      this.sprite.setVelocityX(spriteSpeed)
     } else {
-      this.sprite.setVelocityX(0);
-      this.stateMachine.setState('idle');
+      this.sprite.setVelocityX(0)
+      this.stateMachine.setState('idle')
     }
 
-    const jumpPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space);
+    const jumpPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space)
 
-    const pressed = jumpPressed || this.cursors.up.isDown;
+    const pressed = jumpPressed || this.cursors.up.isDown
 
     if (pressed) {
-      this.stateMachine.setState('jump');
+      this.stateMachine.setState('jump')
     }
   }
 
   jumpOnEnter() {
-    this.sprite.setVelocityY(-12);
+    this.sprite.setVelocityY(-12)
   }
 
   jumpOnUpdate() {
-    const spriteSpeed = 5;
+    const spriteSpeed = 5
     if (this.cursors.left.isDown) {
-      this.sprite.flipX = true;
-      this.sprite.setVelocityX(-spriteSpeed);
+      this.sprite.flipX = true
+      this.sprite.setVelocityX(-spriteSpeed)
     } else if (this.cursors.right.isDown) {
-      this.sprite.flipX = false;
-      this.sprite.setVelocityX(spriteSpeed);
+      this.sprite.flipX = false
+      this.sprite.setVelocityX(spriteSpeed)
     }
   }
 
   spikeHitOnEnter() {
-    this.sprite.setVelocityY(-12);
-    const startColor = Phaser.Display.Color.ValueToColor(0xffffff);
-    const endColor = Phaser.Display.Color.ValueToColor(0xff0000);
+    this.sprite.setVelocityY(-12)
+    const startColor = Phaser.Display.Color.ValueToColor(0xffffff)
+    const endColor = Phaser.Display.Color.ValueToColor(0xff0000)
     this.scene.tweens.addCounter({
       from: 0,
       to: 100,
@@ -147,27 +148,27 @@ export default class PlayerController {
       yoyo: true,
       ease: Phaser.Math.Easing.Sine.InOut,
       onUpdate: (tween) => {
-        const value = tween.getValue();
+        const value = tween.getValue()
         const colorObject = Phaser.Display.Color.Interpolate.ColorWithColor(
           startColor,
           endColor,
           100,
-          value,
-        );
-        const color = Phaser.Display.Color.GetColor(colorObject.r, colorObject.g, colorObject.b);
-        this.sprite.setTint(color);
+          value
+        )
+        const color = Phaser.Display.Color.GetColor(colorObject.r, colorObject.g, colorObject.b)
+        this.sprite.setTint(color)
       },
-    });
+    })
 
-    this.stateMachine.setState('idle');
-    this.setHealth(this.health - 10);
+    this.stateMachine.setState('idle')
+    this.setHealth(this.health - 40)
   }
 
   createAnimations() {
     this.sprite.anims.create({
       key: 'player-idle',
       frames: [{ key: 'kim', frame: 'player_run_5.png' }],
-    });
+    })
 
     this.sprite.anims.create({
       key: 'player-run',
@@ -179,7 +180,7 @@ export default class PlayerController {
         suffix: '.png',
       }),
       repeat: -1,
-    });
+    })
 
     this.sprite.anims.create({
       key: 'player-die',
@@ -190,6 +191,6 @@ export default class PlayerController {
         suffix: '.png',
       }),
       frameRate: 10,
-    });
+    })
   }
 }
